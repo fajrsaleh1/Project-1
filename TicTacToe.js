df515b6e4f84ddce2dd0e34724d938e7effbd9c7
fajrsaleh1 <fajrsalbakiri@gmail.com>
2019-01-29 12:28:17 +0300

    The Final ver of the Game

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,13 +1,17 @@
 
+// To switch between two users I used even and odd numbers X --> odd and O --> even 
+
 var player = 1;
 function startGame(event) {
- 
+
+
     // I need to check it the cell is empty or not
      if ($(event.target).text() === '') { 
         if(player%2 === 1){
             $(event.target).text('X');
             $(event.target).css({"color":"white", "font-size":"110px"});
             $('.whosNext').text('Next move by O');
+            //to add a sound when a user click the cell
             $("<audio></audio>").attr({
                 'src':'sound.wav',
                 'volume':0.4,
@@ -15,17 +19,20 @@ function startGame(event) {
             }).appendTo("body");
 
          }
-        else { 
-            $(event.target).text('O');
-            $(event.target).css({"color":"white", "font-size":"110px"});
+        else {
+            
+           $(event.target).text('O');
+           $(event.target).css({"color":"white", "font-size":"110px"});
             $('.whosNext').text('Next move by X');
+            //to add a sound when a user click the cell
             $("<audio></audio>").attr({
                 'src':'sound.wav',
                 'volume':0.4,
                 'autoplay':'autoplay'
             }).appendTo("body");
             }
-            player++;
+            player++; 
+            //call the Winner() method
             theWinner()
     }}
 
@@ -33,6 +40,7 @@ $('.grid').on('click', startGame);
 
 function theWinner() {
 
+// identify each cell and get the value of each cell using .text()
 var box1 = $('#gridOne').text();
 var box2 = $('#gridTwo').text();
 var box3 = $('#gridThree').text();
@@ -82,24 +90,23 @@ else if ((box3 === box5) && (box5 === box7) && (box5 !== "")) {
     swal("The winner is " + box7);
     resetField()
 
+//if all the cells have a value and theres no winner 
 } else if((box1 !== '') && (box2 !== '') && (box3 !== '') && (box4 !== '') && (box5 !== '') && (box6 !== '') && (box7 !== '') && (box8 !== '') && (box9 !== '')) {
+    //swal is like alert but with special effect
     swal("No Winner !!! ");
     resetField()
 }
 }
 
 function resetField(){
+    //reset everything
     player = 1;
-    $('#gridOne').text('');
-    $('#gridTwo').text('');
-    $('#gridThree').text('');
-    $('#gridFour').text('');
-    $('#gridFive').text('');
-    $('#gridSix').text('');
-    $('#gridSeven').text('');
-    $('#gridEight').text('');
-    $('#gridNine').text('');
+    countO = 0;
+    countX = 0;
+    $('.grid').text('');
     $('.whosNext').text('');
+
+
 }
 $('#reset').on('click',resetField);
 
